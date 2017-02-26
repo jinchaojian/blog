4046567b86db403e4565f0856db65774c5652616
jinchaojian <1139883346@qq.com>
2017-02-26 22:33:03 +0800

    博客前端页面基本搭建，粒子特效完成

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -10219,170 +10219,6 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
-/**
- * by sam@qunee.com
- */
-;(function ($){
-  'use strict';
-  function _isNumber(n) {
-    return n instanceof Number || (typeof n == "number");
-  }
-  $.fn.borderLayout = function() {
-    var setBounds = function(element, bounds){
-      element.style.position = 'absolute';
-      element.style.boxSizing = 'border-box';
-      for(var name in bounds){
-        var v = bounds[name];
-        if(_isNumber(v)){
-          v = parseInt(v) + 'px';
-        }
-        element.style[name] = v;
-      }
-      $(element).trigger('size.change');
-    };
-    var toNumber = function(sNumber, sum){
-      if(sNumber[sNumber.length - 1] === '%'){
-        return sum * parseInt(sNumber) / 100;
-      }
-      return parseInt(sNumber);
-    }
-    var calculateLength = function(sNumber, sum, min, max){
-      var n = toNumber(sNumber, sum);
-      if(min){
-        min = toNumber(min, sum);
-        if(n < min){
-          return min;
-        }
-      }
-      if(max){
-        max = toNumber(max, sum);
-        if(n > max){
-          return max;
-        }
-      }
-      return n;
-    };
-    return this.each(function() {
-      this.style.boxSizing = 'border-box';
-      this.style.overflow = 'hidden';
-      if(this == document.body || $(this).hasClass('layout--body')){
-        setBounds(this, {top: 0, bottom: 0, left: 0, right: 0})
-      }
-      var isH = $(this).hasClass('layout--h');
-
-      var width = this.clientWidth;
-      var height = this.clientHeight;
-      var i = 0;
-      var children = this.children;
-      var center, north, south, east, west;
-      while(i < children.length){
-        var child = children[i++];
-        var data = child.getAttribute('data-options');
-        if(!data){
-          continue;
-        }
-        //http://stackoverflow.com/questions/4210160/safely-parsing-a-json-string-with-unquoted-keys
-        data = data.replace(/(['"])?([a-zA-Z0-9\-]+)(['"])?:/g, '"$2":');
-        data = data.replace(/'/g, '"');
-        data = '{' + data + '}';
-        try{
-          data = JSON.parse(data);
-        }catch(error){
-          continue;
-        }
-        var region = data.region;
-        if(!region){
-          continue;
-        }
-        child._data = data;
-        if(/center/i.test(region)){
-          center = child;
-          continue;
-        }
-        if(/north/i.test(region)){
-          north = child;
-          continue;
-        }
-        if(/south/i.test(region)){
-          south = child;
-          continue;
-        }
-        if(/east/i.test(region)){
-          east = child;
-          continue;
-        }
-        if(/west/i.test(region)){
-          west = child;
-        }
-      }
-      var widthRest = width, heightRest = height, top = 0, left = 0, temp, temp2;
-      function setWestAndEast(){
-        if(west){
-          temp = west._data.width;
-          if(temp){
-            temp = calculateLength(temp, width, west._data['min-width'], west._data['max-width']);
-            left = temp;
-            temp2 = parseInt(west._data.left) || 0;
-            if(temp2){
-              widthRest -= temp2;
-              left += temp2;
-            }
-            widthRest -= temp;
-            setBounds(west, {top: top, left: temp2, width: temp, height: heightRest});
-          }
-        }
-        if(east){
-          temp = east._data.width;
-          if(temp){
-            temp = calculateLength(temp, width, east._data['min-width'], east._data['max-width']);
-            temp2 = parseInt(east._data.right) || 0;
-            if(temp2){
-              widthRest -= temp2;
-            }
-            widthRest -= temp;
-            setBounds(east, {top: top, right: temp2, width: temp, height: heightRest});
-          }
-        }
-      }
-      function setNorthAndSouth(){
-        if(north){
-          temp = north._data.height;
-          if(temp){
-            temp = calculateLength(temp, height, north._data['min-height'], north._data['max-height']);
-            heightRest -= temp;
-            top = temp;
-            setBounds(north, {top: 0, left: left, width: widthRest, height: temp});
-          }
-        }
-        if(south){
-          temp = south._data.height;
-          if(temp){
-            temp = calculateLength(temp, height, south._data['min-height'], south._data['max-height']);
-            heightRest -= temp;
-            setBounds(south, {bottom: 0, left: left, height: temp, width: widthRest});
-          }
-        }
-      }
-      if(isH){
-        setWestAndEast();
-        setNorthAndSouth();
-      }else{
-        setNorthAndSouth();
-        setWestAndEast();
-      }
-      if(center){
-        setBounds(center, {top: top, left: left, width: widthRest, height: heightRest});
-      }
-    });
-  };
-  $(function(){
-    $('.layout').borderLayout();
-    $(window).resize(function(){
-      $('.layout').borderLayout();
-    });
-  });
-})(jQuery);
-
 /*!
  * Bootstrap v3.3.7 (http://getbootstrap.com)
  * Copyright 2011-2016 Twitter, Inc.
@@ -12761,6 +12597,46 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-  $("p").click(function(){
-    $(this).hide();
-  });
+! function() {
+    function o(w, v, i) {
+        return w.getAttribute(v) || i }
+
+    function j(i) {
+        return document.getElementsByTagName(i) }
+
+    function l() {
+        var i = j("script"),
+            w = i.length,
+            v = i[w - 1];
+        return { l: w, z: o(v, "zIndex", 20), o: o(v, "opacity", 0.5), c: o(v, "color", "0,0,0"), n: o(v, "count", 99) } }
+
+    function k() { r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight }
+
+    function b() { e.clearRect(0, 0, r, n);
+        var w = [f].concat(t);
+        var x, v, A, B, z, y;
+        t.forEach(function(i) { i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
+            for (v = 0; v < w.length; v++) { x = w[v];
+                if (i !== x && null !== x.x && null !== x.y) { B = i.x - x.x, z = i.y - x.y, y = B * B + z * z;
+                    y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke()) } }
+            w.splice(w.indexOf(i), 1) }), m(b) }
+    var u = document.createElement("canvas"),
+        s = l(),
+        c = "c_n" + s.l,
+        e = u.getContext("2d"),
+        r, n, m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(i) { window.setTimeout(i, 1000 / 45) },
+        a = Math.random,
+        f = { x: null, y: null, max: 20000 };
+    u.id = c;
+    u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
+    j("body")[0].appendChild(u);
+    k(), window.onresize = k;
+    window.onmousemove = function(i) { i = i || window.event, f.x = i.clientX, f.y = i.clientY }, window.onmouseout = function() { f.x = null, f.y = null };
+    for (var t = [], p = 0; s.n > p; p++) {
+        var h = a() * r,
+            g = a() * n,
+            q = 2 * a() - 1,
+            d = 2 * a() - 1;
+        t.push({ x: h, y: g, xa: q, ya: d, max: 6000 }) }
+    setTimeout(function() { b() }, 100) }();
+
