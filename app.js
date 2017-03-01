a3102a642c4c5394985f3715d9775a2f63a923e2
jinchaojian <1139883346@qq.com>
2017-03-01 10:44:47 +0800

    实现express request取数据，取回数据后用ejs渲染

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var request = require('request');
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 var register = require('./routes/register');
+
+//var posts=require('./routes/posts');
 
 var app = express();
 
@@ -25,6 +30,44 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+/*app.get('/', function(req, res) {
+  res.send('hello, express');
+});*/
+/*app.get('/posts',function(req,res,next){
+    res.render('posts', { title: '登陆' });
+});*/
+
+app.get('/posts', function(req, res, next) {
+    //res.render('posts', { title: '登陆' });
+    request('http://localhost/chac_blog_api/public/index.php/chac/user/login/?&nickname=242p2&password=11', function(error, response, body) {
+        if (!error) {
+            var result = JSON.parse(body);
+            if (result.code == 200) {
+                res.render('posts', {
+                    'path':'',
+                    result: result.data,
+                    'title':result.code
+                });
+            } else{
+                res.render('error', {
+                    'message': result.message,
+                    error: {
+                        'status': result.code,
+                        'stack': ''
+                    }
+                });
+            }
+        } else {
+            console.error('posts failed:', error);
+            next(error);
+        }
+    })
+});
+
+
+
+
+
 app.use('/users', users);
 app.use('/login',login);
 app.use('/register',register);
@@ -47,4 +90,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//新增测试修改路由请求
+app.listen(3000);
 module.exports = app;
